3793798a41b717e76bd07b736231699a693c9deb
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-06-14 14:22:28 -0500

    initial push

---
+++ src/main/java/com/example/wagster/WagsterApplication.java
@@ -0,0 +1,13 @@
+package com.example.wagster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WagsterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wagster/WagsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wagster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
