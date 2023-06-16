640c78dac12a7f3d6c8092339fab270097a04776
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-06-16 12:17:48 -0500

    finished the models

--- src/main/java/com/example/wagster/models/Post.java
+++ src/main/java/com/example/wagster/models/Post.java
@@ -1,4 +1,35 @@
-//package com.example.wagster.models;
-//
-//public class Post {
-//}
+package com.example.wagster.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter@Setter
+@Entity
+@Table( name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String body;
+
+    @Column(nullable = false, columnDefinition = "LONGBLOB")
+    @Basic(fetch = FetchType.LAZY)
+    @Lob
+    private Blob file;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
--- src/main/java/com/example/wagster/models/Review.java
+++ src/main/java/com/example/wagster/models/Review.java
@@ -1,4 +1,33 @@
-//package com.example.wagster.models;
-//
-//public class Review {
-//}
+package com.example.wagster.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name =  "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private short rating;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "locaton_id")
+    private Location location;
+}
