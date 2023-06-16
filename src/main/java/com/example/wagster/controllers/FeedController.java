b6ba0b9fd5ab7d57067f66eb3e631b6e4c889c76
Alex Perez <alex.f.perez92@gmail.com>
2023-06-16 11:52:48 -0500

    made basic controllers and htmls for all the main pages

---
+++ src/main/java/com/example/wagster/controllers/FeedController.java
@@ -0,0 +1,13 @@
+package com.example.wagster.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FeedController {
+
+    @GetMapping("/feed")
+    public String profile() {
+        return "post/feed";
+    }
+}
