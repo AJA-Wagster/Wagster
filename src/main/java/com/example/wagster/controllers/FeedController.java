e8cc2e27133660e7f5c9304743fed9d726d4aff4
auriel grant <aurgrant@gmail.com>
2023-06-21 15:08:01 -0500

    pushing my changes for the day

--- src/main/java/com/example/wagster/controllers/FeedController.java
+++ src/main/java/com/example/wagster/controllers/FeedController.java
@@ -1,15 +1,46 @@
 package com.example.wagster.controllers;
 
+import com.example.wagster.models.Event;
+import com.example.wagster.models.Post;
+import com.example.wagster.repos.EventRepo;
+import com.example.wagster.repos.PostRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 //@Controller
 //public class FeedController {
 //
-//    @GetMapping("/feed")
-//    public String profile() {
-//        return "posts/feed";
-//    }
+
 //}
 
+@Controller
+public class FeedController {
+
+    private final EventRepo eventsDao;
+    private PostRepo postsDao;
+
+    public FeedController(EventRepo eventsDao, PostRepo postsDao) {
+        this.eventsDao = eventsDao;
+        this.postsDao = postsDao;
+    }
+
+    @GetMapping("/feed")
+    public String showFeed(Model model) {
+
+//        get the events from the event service
+        List<Event> events = eventsDao.findAll();
+//        get the posts from the post service
+        List<Post> posts = postsDao.findAll();
+
+//        add events and posts to the feed
+        model.addAttribute("events", events);
+        model.addAttribute("posts", posts);
+
+
+        return "posts/feed";
+    }
+}
 
