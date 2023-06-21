9c5494d8b5e69cbca1998dba5072debcaa5bc026
auriel grant <aurgrant@gmail.com>
2023-06-21 14:12:25 -0500

    feed controller

--- src/main/java/com/example/wagster/controllers/FeedController.java
+++ src/main/java/com/example/wagster/controllers/FeedController.java
@@ -3,11 +3,13 @@ package com.example.wagster.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class FeedController {
+//@Controller
+//public class FeedController {
+//
+//    @GetMapping("/feed")
+//    public String profile() {
+//        return "posts/feed";
+//    }
+//}
 
-    @GetMapping("/feed")
-    public String profile() {
-        return "posts/feed";
-    }
-}
+
