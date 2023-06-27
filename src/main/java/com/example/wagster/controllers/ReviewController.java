3a4260fb3e3c209bbc75b607dd37d429cdd4b73b
Alex Perez <alex.f.perez92@gmail.com>
2023-06-27 10:58:54 -0500

    added cards to the map html

--- src/main/java/com/example/wagster/controllers/ReviewController.java
+++ src/main/java/com/example/wagster/controllers/ReviewController.java
@@ -1,22 +1,22 @@
-package com.example.wagster.controllers;
-
-import com.example.wagster.repos.ReviewRepo;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
-public class ReviewController {
-
-    private final ReviewRepo reviewDao;
-
-    public ReviewController(ReviewRepo reviewDao) {
-        this.reviewDao = reviewDao;
-    }
-
-    @GetMapping("/review/create")
-    @PostMapping("/review/create")
-    @GetMapping("/review/{id}/edit")
-    @PostMapping("/review/{id}/edit")
-    @PostMapping("/review/{id}/delete")
-}
+//package com.example.wagster.controllers;
+//
+//import com.example.wagster.repos.ReviewRepo;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//public class ReviewController {
+//
+//    private final ReviewRepo reviewDao;
+//
+//    public ReviewController(ReviewRepo reviewDao) {
+//        this.reviewDao = reviewDao;
+//    }
+//
+//    @GetMapping("/review/create")
+//    @PostMapping("/review/create")
+//    @GetMapping("/review/{id}/edit")
+//    @PostMapping("/review/{id}/edit")
+//    @PostMapping("/review/{id}/delete")
+//}
