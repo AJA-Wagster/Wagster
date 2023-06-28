5c044678c131c30f41394c624d1e869d4223f6e0
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-06-28 09:27:23 -0500

    map controller

--- src/main/java/com/example/wagster/repos/ReviewRepo.java
+++ src/main/java/com/example/wagster/repos/ReviewRepo.java
@@ -3,5 +3,8 @@ package com.example.wagster.repos;
 import com.example.wagster.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewRepo extends JpaRepository<Review, Long> {
+    void delete(Optional<Review> byId);
 }
