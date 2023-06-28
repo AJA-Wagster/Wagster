c3b1f8b6cd7b919da306cc440a2175411f0944d6
Alex Perez <alex.f.perez92@gmail.com>
2023-06-28 09:34:13 -0500

    edit/ delete functionality

--- src/main/java/com/example/wagster/controllers/FeedController.java
+++ src/main/java/com/example/wagster/controllers/FeedController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -59,16 +60,27 @@ public class FeedController {
     }
 
     @GetMapping("/posts/{id}/edit")
-    public String showPostEditForm(){
+    public String showPostEditForm(@PathVariable("id") Long id, Model model){
+        Post post = postDao.findById(id).orElse(null);
+        model.addAttribute("post", post);
         return "posts/postEdit";
     }
+
     @PostMapping("/posts/{id}/edit")
-    public String updatePostDB(){
+    public String updatePostDB(@PathVariable("id") Long id, @ModelAttribute Post updatedPost){
+        Post post = postDao.findById(id).orElse(null);
+        if (post != null) {
+            post.setTitle(updatedPost.getTitle());
+            post.setBody(updatedPost.getBody());
+            // Update other properties as needed
+            postDao.save(post);
+        }
         return "redirect:/feed";
     }
 
-    @PostMapping("/post/{id}/delete")
-    public String removePostFromDB(){
+    @PostMapping("/posts/{id}/delete")
+    public String removePostFromDB(@PathVariable("id") Long id){
+        postDao.deleteById(id);
         return "redirect:/feed";
     }
 
