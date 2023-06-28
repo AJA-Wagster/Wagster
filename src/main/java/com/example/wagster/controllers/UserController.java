f80290acfb76bbb6a81867bbde81a457963dbb8b
auriel grant <aurgrant@gmail.com>
2023-06-28 09:27:18 -0500

    feed controller updated i added admin deleting a user

--- src/main/java/com/example/wagster/controllers/UserController.java
+++ src/main/java/com/example/wagster/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.example.wagster.models.User;
 import com.example.wagster.repos.UserRepo;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -83,4 +84,43 @@ public String deleteProfile(@ModelAttribute("user") User userToDelete, HttpServl
     }
     return "redirect:/";
     }
+
+    @GetMapping("/admin/users/{id}/delete")
+    public String showDeleteUserForm(@PathVariable Long id, Model model) {
+        // Retrieve the user to be deleted from the userRepo
+        User userToDelete = userDao.findById(id).orElse(null);
+
+        // Pass the user object to the view
+        model.addAttribute("user", userToDelete);
+
+        return "users/deleteProfile";
+    }
+
+    @PostMapping("/admin/users/{id}/delete")
+    public String deleteUser(@PathVariable Long id, HttpServletRequest request) {
+        // Retrieve the user to be deleted from the userRepo
+        User userToDelete = userDao.findById(id).orElse(null);
+
+        // Perform authorization check here to ensure admin rights
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isAdmin = authentication.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
+        if (!isAdmin) {
+            // Redirect or show an error message indicating insufficient privileges
+            return "redirect:/admin/users"; // Redirect to admin user list
+        }
+
+        // Delete the user from the userRepo
+        userDao.deleteById(id);
+
+        // Invalidate the current session
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/admin/users";
+    }
+
+
 }
