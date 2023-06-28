b5b9e5f65ffb77fddea02ad9e18d43c8c65368a2
auriel grant <aurgrant@gmail.com>
2023-06-28 10:27:29 -0500

    merging

--- src/main/java/com/example/wagster/controllers/UserController.java
+++ src/main/java/com/example/wagster/controllers/UserController.java
@@ -34,18 +34,19 @@ public class UserController  {
 
     @PostMapping("/register")
     public String registerUser(@ModelAttribute("user") User user) {
-        // Perform registration logic here using userRepo
-        // e.g., save the user to the database using userRepo.save(user)
-
         String hash = passwordEncoder.encode(user.getPassword());
-//        set the hashed password BEFORE saving to the database
         user.setPassword(hash);
+
+        // Assign the "ROLE_ADMIN" authority to the user
+        user.setRole("ROLE_ADMIN");
+
         userDao.save(user);
 
         return "redirect:/";
     }
 
-@GetMapping("/profile/edit")
+
+    @GetMapping("/profile/edit")
     public String showEditProfileForm(Model model){
     User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
@@ -85,42 +86,6 @@ public String deleteProfile(@ModelAttribute("user") User userToDelete, HttpServl
     return "redirect:/";
     }
 
-    @GetMapping("/admin/users/{id}/delete")
-    public String showDeleteUserForm(@PathVariable Long id, Model model) {
-        // Retrieve the user to be deleted from the userRepo
-        User userToDelete = userDao.findById(id).orElse(null);
-
-        // Pass the user object to the view
-        model.addAttribute("user", userToDelete);
-
-        return "users/deleteProfile";
-    }
-
-    @PostMapping("/admin/users/{id}/delete")
-    public String deleteUser(@PathVariable Long id, HttpServletRequest request) {
-        // Retrieve the user to be deleted from the userRepo
-        User userToDelete = userDao.findById(id).orElse(null);
-
-        // Perform authorization check here to ensure admin rights
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        boolean isAdmin = authentication.getAuthorities().stream()
-                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
-        if (!isAdmin) {
-            // Redirect or show an error message indicating insufficient privileges
-            return "redirect:/admin/users"; // Redirect to admin user list
-        }
-
-        // Delete the user from the userRepo
-        userDao.deleteById(id);
-
-        // Invalidate the current session
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-
-        return "redirect:/admin/users";
-    }
 
 
 }
